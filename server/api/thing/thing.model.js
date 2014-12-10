ef0123ab41f06ab4c5bbc9fe5c1474063ac8e92c
Yasuki Ikeuchi <ikeyasu@gmail.com>
2014-12-11 08:47:09 +0900

    Add value field to Thing model

--- server/api/thing/thing.model.js
+++ server/api/thing/thing.model.js
@@ -4,9 +4,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var ThingSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  name: {type: String, index: true},
+  value: Number,
+  date: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Thing', ThingSchema);
