479ff853203e4728def6bc05c0c4524c002b3707
Yasuki Ikeuchi <ikeyasu@gmail.com>
2014-12-14 00:05:55 +0900

    remove unnecessary methods

--- server/api/thing/index.js
+++ server/api/thing/index.js
@@ -8,10 +8,5 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/retrieveSensors', controller.retrieveSensors);
 router.get('/showSensor', controller.showSensor);
-router.get('/:id', controller.show);
-router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
 
 module.exports = router;
